f92df66077e3578cbb6b07696e1216874506966f
mdshowkatislam <showkattuhin88766@gmail.com>
2025-07-09 19:52:41 +0600

    group table created

---
+++ database/migrations/2025_07_09_195112_groups.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('group_name')->unique();
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('shift_id')->nullable();
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps();
+
+            $table->foreign('shift_id')->references('id')->on('shift_settings')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('groups');
+    }
+};
